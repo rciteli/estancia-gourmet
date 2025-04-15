320211bcbbc7499b0c8d8b14b6104c1bed715758
rciteli <rciteli@gmail.com>
2025-04-14 22:18:30 -0300

    correcao erro footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,8 @@
 // components/Footer.tsx
 export default function Footer() {
     return (
-      <footer className="bg-emerald-500 text-white py-2 lohit">
-        <div className="container mx-auto text-center">
+      <footer className="bg-emerald-500 text-white py-2">
+        <div className="container mx-auto text-center lohit">
           <p>© 2025 – Todos os direitos reservados a Estância Gourmet. | Site desenvolvido por ARC Group.</p>
         </div>
       </footer>
